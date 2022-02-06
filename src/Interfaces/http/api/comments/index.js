34257de0b487a8e67bd5e41294facd39693d120f
Sigadong <irwanto.regar30@gmail.com>
2022-02-06 21:18:27 +0700

    passed -1

--- src/Interfaces/http/api/comments/index.js
+++ src/Interfaces/http/api/comments/index.js
@@ -2,7 +2,7 @@ const CommentsHandler = require('./handler');
 const routes = require('./routes');
 
 module.exports = {
-  name: 'commentss',
+  name: 'comments',
   register: async (server, { container }) => {
     const commentsHandler = new CommentsHandler(container);
     server.route(routes(commentsHandler));
