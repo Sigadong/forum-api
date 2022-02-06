0b303a0cc0290529fb584ec71e566b2ca91edee7
Sigadong <irwanto.regar30@gmail.com>
2022-02-06 22:03:14 +0700

    passed

--- src/Infrastructures/repository/ThreadRepositoryPostgres.js
+++ src/Infrastructures/repository/ThreadRepositoryPostgres.js
@@ -5,6 +5,7 @@ const ThreadRepository = require('../../Domains/threads/ThreadRepository');
 class ThreadRepositoryPostgres extends ThreadRepository {
   constructor(pool, idGenerator) {
     super();
+    this._pool = pool;
     this._idGenerator = idGenerator;
   }
 
