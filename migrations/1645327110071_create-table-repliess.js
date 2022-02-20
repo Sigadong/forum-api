10c726e6a4a9b875611792d0d6529020f8d2e05f
Sigadong <irwanto.regar30@gmail.com>
2022-02-20 11:24:15 +0700

    modified migrations

--- migrations/1645327110071_create-table-repliess.js
+++ migrations/1645327110071_create-table-repliess.js
@@ -27,15 +27,15 @@ exports.up = (pgm) => {
     },
   });
 
+  pgm.addConstraint('replies', 'fk_replies.owner_users.id', 'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE');
   pgm.addConstraint('replies', 'fk_replies.thread_id_threads.id', 'FOREIGN KEY(thread_id) REFERENCES threads(id) ON DELETE CASCADE');
   pgm.addConstraint('replies', 'fk_replies.comment_id_comments.id', 'FOREIGN KEY(comment_id) REFERENCES comments(id) ON DELETE CASCADE');
-  pgm.addConstraint('replies', 'fk_replies.owner_users.id', 'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE');
 };
 
 exports.down = (pgm) => {
+  pgm.dropConstraint('replies', 'fk_replies.owner_users.id');
   pgm.dropConstraint('replies', 'fk_replies.thread_id_threads.id');
   pgm.dropConstraint('replies', 'fk_replies.comment_id_comments.id');
-  pgm.dropConstraint('replies', 'fk_replies.owner_users.id');
 
   pgm.dropTable('replies');
 };
