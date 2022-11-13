bc0ab141a7d76324210e7db5353d58bc6ba49637
Sigadong <irwanto.regar30@gmail.com>
2022-11-13 17:51:03 +0700

    ..

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const createServer = require('./Infrastructures/http/createServer');
 const container = require('./Infrastructures/container');
-
+// server
 (async () => {
   const server = await createServer(container);
   await server.start();
